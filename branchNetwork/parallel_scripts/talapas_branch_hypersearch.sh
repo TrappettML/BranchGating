861c70a601705cbb48a6deb4a1c5790fbd8f934d
Matthew Trappett <mtrappet@uoregon.edu>
2024-05-05 13:49:30 -0700

    update branch .sh

--- branchNetwork/parallel_scripts/talapas_branch_hypersearch.sh
+++ branchNetwork/parallel_scripts/talapas_branch_hypersearch.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name BranchSearch --command "python ../experiments/hyper_search_branch.py" --num-nodes 20 --partition "compute" --days 1
+python launch.py --exp-name BranchSearch --command "python ../experiments/hyper_search_branch.py" --num-nodes 20 --partition "computelong" --days 14
