2473fa0a4f95d4da8e440bb64319bf3815e25a6c
Matthew Trappett <mtrappet@uoregon.edu>
2024-05-22 09:08:00 -0700

    changed permute parallel to computelong_intel

--- branchNetwork/parallel_scripts/permute_talapas_branch_hypersearch.sh
+++ branchNetwork/parallel_scripts/permute_talapas_branch_hypersearch.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name PermuteBranch --command "python ../experiments/permute_branch_search.py" --num-nodes 4 --partition "compute_intel" --days 1
+python launch.py --exp-name PermuteBranch --command "python ../experiments/permute_branch_search.py" --num-nodes 6 --partition "computelong_intel" --days 14
