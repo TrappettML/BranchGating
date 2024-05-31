13205143afc46f389ded961bcb0fff2213577216
Matthew Trappett <mtrappet@uoregon.edu>
2024-05-31 09:29:23 -0700

    changed the learn .sh

--- branchNetwork/parallel_scripts/learn_gates.sh
+++ branchNetwork/parallel_scripts/learn_gates.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name BranchFuncExperi --command "python ../experiments/GateLearning.py" --num-nodes 3 --partition "compute" --days 1
+python launch.py --exp-name BranchFuncExperi --command "python ../experiments/GateLearning.py" --num-nodes 3 --partition "computelong_intel" --days 14
