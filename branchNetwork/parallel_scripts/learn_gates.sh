1023806722ee43ee972a72da38208f727626e8e7
Matthew Trappett <mtrappet@uoregon.edu>
2024-05-31 09:39:11 -0700

    using compute partition

--- branchNetwork/parallel_scripts/learn_gates.sh
+++ branchNetwork/parallel_scripts/learn_gates.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name BranchFuncExperi --command "python ../experiments/GateLearning.py" --num-nodes 3 --partition "computelong" --days 14
+python launch.py --exp-name BranchFuncExperi --command "python ../experiments/GateLearning.py" --num-nodes 3 --partition "compute" --days 1
