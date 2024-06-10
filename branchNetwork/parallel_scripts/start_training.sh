2ae2321b255abbd0f1c171a53e95fee072bcd046
Matthew Trappett <mtrappet@uoregon.edu>
2024-06-10 12:26:56 -0700

    changed start_training

--- branchNetwork/parallel_scripts/start_training.sh
+++ branchNetwork/parallel_scripts/start_training.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name LongSparseRotate --command "python ../experiments/LongLearning.py" --num-nodes 30 --partition "computelong_intel" --days 14
+python launch.py --exp-name LongSparseRotate --command "python ../experiments/LongLearning.py" --num-nodes 20 --partition "compute_intel" --days 1
