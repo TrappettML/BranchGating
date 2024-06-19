780146f1c32aa13fc9a63b507e3d102ea167630e
Matthew Trappett <mtrappet@uoregon.edu>
2024-06-19 06:55:18 -0700

    changed to compute_long

--- branchNetwork/parallel_scripts/start_training.sh
+++ branchNetwork/parallel_scripts/start_training.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name SomaFuncSearchMax --command "python ../experiments/LongLearning.py" --num-nodes 15 --partition "computelong_intel" --days 14
+python launch.py --exp-name SomaFuncSearchMax --command "python ../experiments/LongLearning.py" --num-nodes 15 --partition "compute_intel" --days 1
