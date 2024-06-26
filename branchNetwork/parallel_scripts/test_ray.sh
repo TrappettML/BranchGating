925911e22dbba854b33537e5042464ee8a637bca
Matthew Trappett <mtrappet@uoregon.edu>
2024-06-26 11:58:33 -0700

    test compute

--- branchNetwork/parallel_scripts/test_ray.sh
+++ branchNetwork/parallel_scripts/test_ray.sh
@@ -1,1 +1,1 @@
-python launch.py --exp-name gpu_test_ray --command "python test_ray.py" --num-nodes 2 --partition "gpu" --num-gpus 3
+python launch.py --exp-name compute_test_ray --command "python test_ray.py" --num-nodes 2 --partition "computelong" --num-gpus 3
