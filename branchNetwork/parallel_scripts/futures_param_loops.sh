93d1ac0ea7d9a40ccb89973239dd4667a33cc894
Matthew Trappett <mtrappet@uoregon.edu>
2025-01-06 15:25:24 -0800

    different sparsity values 0.5

--- branchNetwork/parallel_scripts/futures_param_loops.sh
+++ branchNetwork/parallel_scripts/futures_param_loops.sh
@@ -2,7 +2,7 @@
 # param_loops.sh
 
 model_names=('Branch')
-branch_nums=(1 2 7 14 28 49)
+branch_nums=(1)
 # learning_rates=(0.1 0.01 0.001 0.0001 0.00001)
 # branch_nums=(1 2 5 10 20 50 100 200 400 800 1200)
 # soma_funcs=('tanh' 'sigmoid' 'softplus' 'softsign' 'elu' 'gelu' 'selu')
@@ -11,7 +11,7 @@ sparsities=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0) #(0.0 0.5) #
 repeats=(1 2 3 4 5)
 hiddens=(784) # (50 100 200 400)
 n_npbs=(1) # 5 10 20 50 100 200 400 800 1200)
-lrules=('rl')
+lrules=('sl')
 det_gates=(0 1)
 
 for model in "${model_names[@]}"; do
